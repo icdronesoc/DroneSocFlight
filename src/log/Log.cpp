926a5dc607147cc1bee5ebbddcf21594633b3e0c
Harry Phillips <harryjamesphillips@gmail.com>
2021-03-11 18:41:50 +0000

    Logger vararg bugfix

--- src/log/Log.cpp
+++ src/log/Log.cpp
@@ -5,39 +5,13 @@
 
 namespace Log {
     namespace { // private
-        constexpr size_t MAX_LOG_MESSAGE_SIZE = 128; // not including prefix
-
         IO::SerialPort* logSerial = nullptr;
         etl::format_spec timestampFormat;
-        bool infoEnabled = false;
-        bool warningEnabled = false;
-        bool errorEnabled = false;
-
-        // logLevel must be 7 chars long.
-        void print(const char* logLevel, const char* tag, const char* format, ...) {
-            if (logSerial != nullptr) {
-                IO::SerialPort& output = *logSerial;
-
-                etl::string<64> messagePrefix = "[";
-                etl::to_string(millis(), messagePrefix, true);
-                messagePrefix.append("] [");
-                messagePrefix.append(logLevel);
-                messagePrefix.append("] [");
-                messagePrefix.append(tag);
-                messagePrefix.append("]: ");
-
-                char buffer[MAX_LOG_MESSAGE_SIZE];
-                va_list va;
-                va_start(va, format);
-                vsnprintf(buffer, MAX_LOG_MESSAGE_SIZE, format, va);
-                va_end(va);
-
-                output->write(messagePrefix.c_str(), messagePrefix.size());
-                output->write(buffer);
-                output->write("\r\n");
-            }
-        }
     }
+
+    bool infoEnabled = true;
+    bool warningEnabled = true;
+    bool errorEnabled = true;
 
     void initialize() {
         timestampFormat.decimal().width(10).fill(' ');
@@ -52,30 +26,21 @@ namespace Log {
         }
     }
 
-    void info(const char* tag, const char *format, ...) {
-        if (infoEnabled) {
-            va_list va;
-            va_start(va, format);
-            print("INFO   ", tag, format, va);
-            va_end(va);
-        }
-    }
+    void print(const char* logLevel, const char* tag, const char* message) {
+        if (logSerial != nullptr) {
+            IO::SerialPort& output = *logSerial;
 
-    void warning(const char* tag, const char *format, ...) {
-        if (warningEnabled) {
-            va_list va;
-            va_start(va, format);
-            print("WARNING", tag, format, va);
-            va_end(va);
-        }
-    }
+            etl::string<64> messagePrefix = "[";
+            etl::to_string(millis(), messagePrefix, timestampFormat, true);
+            messagePrefix.append("] [");
+            messagePrefix.append(logLevel);
+            messagePrefix.append("] [");
+            messagePrefix.append(tag);
+            messagePrefix.append("]: ");
 
-    void error(const char* tag, const char *format, ...) {
-        if (errorEnabled) {
-            va_list va;
-            va_start(va, format);
-            print("ERROR  ", tag, format, va);
-            va_end(va);
+            output->write(messagePrefix.c_str(), messagePrefix.size());
+            output->write(message);
+            output->write("\r\n");
         }
     }
 }
--- src/log/Log.h
+++ src/log/Log.h
@@ -9,9 +9,48 @@ namespace Log {
         ERROR,
     };
 
+    constexpr size_t MAX_LOG_MESSAGE_SIZE = 128;
+
+    extern bool infoEnabled;
+    extern bool warningEnabled;
+    extern bool errorEnabled;
+
     void initialize();
 
-    void info(const char* tag, const char* format, ...);
-    void warning(const char* tag, const char* format, ...);
-    void error(const char* tag, const char* format, ...);
+    /**
+     * Master print function - all other helper functions eventually call this.
+     * Prints to the log.
+     * @param logLevel The log level string
+     * @param tag The tag string
+     * @param message The message string
+     */
+    void print(const char* logLevel, const char* tag, const char* message);
+
+    template <typename ... Args>
+    void print(const char* logLevel, const char* tag, const char* format, const Args&... args) {
+        char buffer[MAX_LOG_MESSAGE_SIZE];
+        snprintf(buffer, MAX_LOG_MESSAGE_SIZE, format, args...);
+        print(logLevel, tag, buffer);
+    }
+
+    template <typename ... Args>
+    void info(const char* tag, const char *format, const Args&... args) {
+        if (infoEnabled) {
+            print("INFO   ", tag, format, args...);
+        }
+    }
+
+    template <typename ... Args>
+    void warning(const char* tag, const char *format, const Args&... args) {
+        if (warningEnabled) {
+            print("WARNING", tag, format, args...);
+        }
+    }
+
+    template <typename ... Args>
+    void error(const char* tag, const char *format, const Args&... args) {
+        if (errorEnabled) {
+            print("ERROR  ", tag, format, args...);
+        }
+    }
 }
